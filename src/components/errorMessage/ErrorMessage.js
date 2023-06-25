32772b0757d7c5b65e3e29f8aade893218d7583a
VVasilevich <vve913@gmail.com>
2023-06-25 18:08:14 +0600

    Add ErrorMessage

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,7 @@
+const ErrorMessage = () => {
+    return (
+        <div style={{ textAlign: 'center', fontSize: '50px', lineHeight: '75px', fontWeight: 'bold'}}>ERROR <br /> Try again</div>
+    )
+}
+
+export default ErrorMessage;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
 
 import './randomChar.scss';
@@ -13,7 +14,8 @@ class RandomChar extends Component {
 
     state = {
         char: {},
-        loading: true
+        loading: true,
+        error: false
     }
 
     marvelService = new MarvelService();
@@ -25,18 +27,32 @@ class RandomChar extends Component {
         })
     }
 
+    onError = () => {
+        this.setState({
+            loading: false,
+            error:true
+        })
+    }
+
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000)
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
+            .catch(this.onError)
     }
 
     render() {
-        const {char, loading} = this.state;
+        const {char, loading, error} = this.state;
+        const content = !(loading || error) ? <View char={char}/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const erroMessage = error ? <ErrorMessage/> : null;
+
         return (
             <div className="randomchar">
-                {loading ? <Spinner/> : <View char={char}/>}
+                {content}
+                {spinner}
+                {erroMessage}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
