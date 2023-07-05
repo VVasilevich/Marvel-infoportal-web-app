e07bb2b0229b189bce247e4efdeb24938b3d8efb
VVasilevich <vve913@gmail.com>
2023-07-05 14:36:30 +0600

    Add focus functionality to CharList

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -17,6 +17,8 @@ class CharList extends Component {
         charEnded: false
     }
 
+    itemRefs = [];
+
     marvelService = new MarvelService();
 
     componentDidMount() {
@@ -59,8 +61,17 @@ class CharList extends Component {
         })
     }
 
+    setRef = (ref) => {
+        this.itemRefs.push(ref)
+    }
+
+    onFocus = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'))
+        this.itemRefs[id].classList.add('char__item_selected')
+    }
+
     renderArr(arr) {
-        const items = arr.map((item) => {
+        const items = arr.map((item, i) => {
             let imgStyle = {'objectFit': 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit': 'unset'};
@@ -69,7 +80,18 @@ class CharList extends Component {
             return (
                 <li className="char__item"
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
+                    tabIndex={0}
+                    ref={this.setRef}
+                    onClick={() => {
+                        this.props.onCharSelected(item.id)
+                        this.onFocus(i)
+                    }}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            this.props.onCharSelected(item.id)
+                            this.onFocus(i)
+                        }
+                    }}>
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
