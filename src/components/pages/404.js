89a5137ae728e9338f3c2c9e69241d50c096c482
VVasilevich <vve913@gmail.com>
2023-07-15 15:32:50 +0600

    Fix styles in Page404

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom"
 
 const Page404 = () => {
     return (
-        <div style={{'display': 'flex', 'justify-content': 'center', 'flex-direction': 'column'}}>
+        <div>
             <p style={{'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '50px'}}>Page Not Found</p>
-            <button className="button button__main" style={{'margin': '60px auto'}}>
+            <button className="button button__main" style={{'display': 'block', 'margin': '60px auto'}}>
                 <Link to="/" className="inner">Back to main page</Link>
             </button>
         </div>
